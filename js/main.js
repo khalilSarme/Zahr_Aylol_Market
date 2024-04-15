4479be819100dda33ec6719316944ad3c37496ae
Khalil.Net <165773486+khalilSarme@users.noreply.github.com>
2024-04-15 22:52:03 +0300

    Updated

--- js/main.js
+++ js/main.js
@@ -1,129 +1,75 @@
-$(document).ready(function() {
-
-  var themeColor = getComputedStyle(
-      document.documentElement)
-    .getPropertyValue('--bx-shadow')
+$(document).ready(function () {
+  var themeColor = getComputedStyle(document.documentElement)
+    .getPropertyValue("--bx-shadow")
     .trim();
-  $('meta[name="theme-color"]')
-    .attr('content', themeColor);
+  $('meta[name="theme-color"]').attr("content", themeColor);
 
   function validate() {
-    var value = $("#search_content")
-      .val().toLowerCase();
-    $(".website_content .container")
-      .filter(function() {
-        $(this).toggle($(this)
-          .text().toLowerCase()
-          .indexOf(value) > -1)
-      });
+    var value = $("#search_content").val().toLowerCase();
+    $(".website_content .container").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
   }
-
-  $(".btn_search").on("click",
-    function() {
-      var value = $(
-          "#search_content").val()
-        .toLowerCase();
-      $(".website_content .container")
-        .filter(function() {
-          $(this).toggle($(this)
-            .text()
-            .toLowerCase()
-            .indexOf(value) >
-            -1)
-        });
-    });
-
-  $(".navbar-toggler").click(
-    function() {
-      $("#navbar").slideToggle(
-        "show");
-    });
-  $(".navbar a").click(function(e) {
-    $(".navbar a.active")
-      .removeClass("active");
-    e.target.classList.add(
-      "active");
+  $(".label_category").click(function() {
+    $(".category_collapse").slideToggle();
   });
-  $(".root-theme").click(
-function() {
-    $("#theme-icon")
-      .toggleClass("fa-sun")
-      .toggleClass("fa-moon");
-    $("html").toggleClass(
-      "dark").toggleClass(
-      "light");
+  $(".btn_search").on("click", function () {
+    var value = $("#search_content").val().toLowerCase();
+    $(".website_content .container").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
   });
 
-  $(".dodger-button").click(
-    function() {
-      $(".light.crimson")
-        .removeClass("crimson")
-        .addClass("dodger");
-      $(".dark.crimson")
-        .removeClass("crimson")
-        .addClass("dodger");
-      themeColor =
-        getComputedStyle(document
-          .documentElement)
-        .getPropertyValue(
-          '--bx-shadow').trim();
-      $('meta[name="theme-color"]')
-        .attr('content',
-          themeColor);
-    });
+  $(".navbar-toggler").click(function () {
+    $("#navbar").slideToggle("show");
+  });
+  $(".navbar a").click(function (e) {
+    $(".navbar a.active").removeClass("active");
+    e.target.classList.add("active");
+  });
+  $(".root-theme").click(function () {
+    $("#theme-icon").toggleClass("fa-sun").toggleClass("fa-moon");
+    $("html").toggleClass("dark").toggleClass("light");
+  });
 
-  $(".crimson-button").click(
-    function() {
-      $(".light.dodger")
-        .removeClass("dodger")
-        .addClass("crimson");
-      $(".dark.dodger")
-        .removeClass("dodger")
-        .addClass("crimson");
-      themeColor =
-        getComputedStyle(document
-          .documentElement)
-        .getPropertyValue(
-          '--bx-shadow').trim();
-      $('meta[name="theme-color"]')
-        .attr('content',
-          themeColor);
-    });
+  $(".dodger-button").click(function () {
+    $(".light.crimson").removeClass("crimson").addClass("dodger");
+    $(".dark.crimson").removeClass("crimson").addClass("dodger");
+    themeColor = getComputedStyle(document.documentElement)
+      .getPropertyValue("--bx-shadow")
+      .trim();
+    $('meta[name="theme-color"]').attr("content", themeColor);
+  });
+
+  $(".crimson-button").click(function () {
+    $(".light.dodger").removeClass("dodger").addClass("crimson");
+    $(".dark.dodger").removeClass("dodger").addClass("crimson");
+    themeColor = getComputedStyle(document.documentElement)
+      .getPropertyValue("--bx-shadow")
+      .trim();
+    $('meta[name="theme-color"]').attr("content", themeColor);
+  });
 
   function setTheme(theme) {
-    localStorage.setItem('theme',
-      theme);
+    localStorage.setItem("theme", theme);
   }
 
-  document.addEventListener(
-    'DOMContentLoaded',
-    function() {
-      const savedTheme =
-        localStorage.getItem(
-          'theme');
-      if (savedTheme) {
-        $("html").setClass(
-          savedTheme);
-      }
-    });
+  document.addEventListener("DOMContentLoaded", function () {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      $("html").setClass(savedTheme);
+    }
+  });
 
-  window.addEventListener(
-    'beforeunload',
-    function() {
-      const currentTheme = $(
-        "html").className;
-      setTheme(currentTheme);
-    });
-
+  window.addEventListener("beforeunload", function () {
+    const currentTheme = $("html").className;
+    setTheme(currentTheme);
+  });
 });
 
 function validate() {
-  var value = $("#search_content").val()
-    .toLowerCase();
-  $(".website_content .container")
-    .filter(function() {
-      $(this).toggle($(this).text()
-        .toLowerCase().indexOf(
-          value) > -1)
-    });
+  var value = $("#search_content").val().toLowerCase();
+  $(".website_content .container").filter(function () {
+    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+  });
 }
