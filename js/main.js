a6b033ca15d5c088ad613c10ecc4cd07a32f7cd2
Khalil.Net <165773486+khalilSarme@users.noreply.github.com>
2024-04-16 20:17:20 +0300

    Updated

--- js/main.js
+++ js/main.js
@@ -1,117 +1,41 @@
 $(document).ready(function() {
-  var themeColor = getComputedStyle(
-      document.documentElement)
-    .getPropertyValue("--bx-shadow")
-    .trim();
-  $('meta[name="theme-color"]')
-    .attr("content", themeColor);
-  $(".arrow").hide();
-  $('.row [class^="col"]').on('click',
-    '.label_category',
-    function() {
-      $(this).siblings(
-          '.category_collapse')
-        .slideToggle();
-    });
-  $(".btn_search").on("click", function () {
-    var value = $("#search_content").val().toLowerCase();
-    $(".website_content .container").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-    });
-  });
 
-  $(".navbar-toggler").click(
-    function() {
-      $("#navbar").slideToggle(
-        "show");
-    });
-  $(".navbar a").click(function(e) {
-    $(".navbar a.active")
-      .removeClass("active");
-    e.target.classList.add(
-      "active");
-  });
-  $(".root-theme").click(
-function() {
-    $("#theme-icon")
-      .toggleClass("fa-sun")
-      .toggleClass("fa-moon");
-    $("html").toggleClass(
-      "dark").toggleClass(
-      "light");
-  });
+    $(".navbar-toggler").click(
+        function() {
+            $("#navbar").slideToggle(
+                "show");
+        });
 
-  $(".dodger-button").click(
-    function() {
-      $(".light.crimson")
-        .removeClass("crimson")
-        .addClass("dodger");
-      $(".dark.crimson")
-        .removeClass("crimson")
-        .addClass("dodger");
-      themeColor =
-        getComputedStyle(document
-          .documentElement)
-        .getPropertyValue(
-          "--bx-shadow")
-        .trim();
-      $('meta[name="theme-color"]')
-        .attr("content",
-          themeColor);
+
+    $(".navbar a").click(function(e) {
+        $(".navbar a.active")
+        .removeClass("active");
+        e.target.classList.add(
+            "active");
     });
 
-  $(".crimson-button").click(
-    function() {
-      $(".light.dodger")
-        .removeClass("dodger")
-        .addClass("crimson");
-      $(".dark.dodger")
-        .removeClass("dodger")
-        .addClass("crimson");
-      themeColor =
-        getComputedStyle(document
-          .documentElement)
-        .getPropertyValue(
-          "--bx-shadow")
-        .trim();
-      $('meta[name="theme-color"]')
-        .attr("content",
-          themeColor);
-    });
 
-  function setTheme(theme) {
-    localStorage.setItem("theme",
-      theme);
-  }
-
-  document.addEventListener(
-    "DOMContentLoaded",
-    function() {
-      const savedTheme =
-        localStorage.getItem(
-          "theme");
-      if (savedTheme) {
-        $("html").setClass(
-          savedTheme);
-      }
-    });
-
-  window.addEventListener(
-    "beforeunload",
-    function() {
-      const currentTheme = $(
-        "html").className;
-      setTheme(currentTheme);
-    });
+    $(".root-theme").click(
+        function() {
+            $("#theme-icon")
+            .toggleClass("fa-sun")
+            .toggleClass("fa-moon");
+            $("html").toggleClass(
+                "dark").toggleClass(
+                "light");
+        });
+        
+        
 });
 
-function validate() {
-  var value = $("#search_content").val()
-    .toLowerCase();
-  $(".website_content .container")
-    .filter(function() {
-      $(this).toggle($(this).text()
-        .toLowerCase().indexOf(
-          value) > -1);
-    });
-}
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/themes.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  var themeColor = getComputedStyle(document.documentElement)
+    .getPropertyValue("--bx-shadow")
+    .trim();
+  $('meta[name="theme-color"]').attr("content", themeColor);
+
+  function changetheme(theme) {
+    var documentRoot = document.querySelector("html").className;
+    if (documentRoot.includes("light")) {
+      if (!documentRoot.includes(theme)) {
+        documentRoot = `${theme} light`;
+        document.querySelector("html").className = documentRoot;
+      }
+    } else if (documentRoot.includes("dark")) {
+      if (!documentRoot.includes(theme)) {
+        documentRoot = `${theme} dark`;
+        document.querySelector("html").className = documentRoot;
+      }
+    } else {
+      return;
+    }
+  }
+
+  let btn_themes = document.querySelectorAll(".btn-theme");
+  btn_themes.item(0).addEventListener("click", () => {
+    changetheme("crimson");
+  });
+  btn_themes.item(1).addEventListener("click", () => {
+    changetheme("blueviolet");
+  });
+  btn_themes.item(2).addEventListener("click", () => {
+    changetheme("dodgerblue");
+  });
+});
