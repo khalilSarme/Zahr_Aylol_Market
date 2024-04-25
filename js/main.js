36d2e93cf0a1e8ce05ff2db8dc7a51ac7809bc91
Khalil.Net <165773486+khalilSarme@users.noreply.github.com>
2024-04-25 13:08:34 +0300

    Updated

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@ $(document).ready(function () {
   $(".navbar-toggler").click(function () {
     $("#navbar").slideToggle("show");
   });
-
-  $(".root_theme_color_setting").click(function () {
-    $(".btn_themes_color_container").toggleClass("tansform_scale");
-  });
-  $(".btn_theme_color").click(function() {
-    $(".btn_themes_color_container").toggleClass("tansform_scale");
-  });
   $(".navbar a").click(function (e) {
     $(".navbar a.active").removeClass("active");
     e.target.classList.add("active");
