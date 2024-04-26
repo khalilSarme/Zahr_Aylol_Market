789063656b8a66968aed2333b2b368c8a8c4e8ea
Khalil.Net <165773486+khalilSarme@users.noreply.github.com>
2024-04-27 01:11:58 +0300

    Updated

--- js/main.js
+++ js/main.js
@@ -2,8 +2,38 @@ $(document).ready(function () {
   $(".navbar-toggler").click(function () {
     $("#navbar").slideToggle("show");
   });
+  $(".btn_theme_setting").click(function() {
+      $(".theme_color_container").toggleClass("scale-0")
+  });
+  $(".btn_theme_color").click(function() {
+      $(".theme_color_container").toggleClass("scale-0");
+  });
   $(".navbar a").click(function (e) {
     $(".navbar a.active").removeClass("active");
     e.target.classList.add("active");
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
