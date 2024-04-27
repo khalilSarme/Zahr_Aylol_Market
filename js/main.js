d96f322e61d0af9ab945e5b06247e6caf6e5fa84
Khalil.Net <165773486+khalilSarme@users.noreply.github.com>
2024-04-27 14:16:11 +0300

    Updated

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
   $(".btn_theme_color").click(function() {
       $(".theme_color_container").toggleClass("scale-0");
   });
+  
+  let images = ["image_01" , "image_02" , "image_03"]
+  
   $(".navbar a").click(function (e) {
     $(".navbar a.active").removeClass("active");
     e.target.classList.add("active");
